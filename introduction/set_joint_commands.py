d54c7937393d82e6afe289bfa3ae997d83c210f3
peeb <93371033+peschenbach@users.noreply.github.com>
2023-05-06 16:36:48 +0200

    Update set_joint_commands.py

--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -16,29 +16,6 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '
 
 from spark_agent import SparkAgent
 
-JOINT_CMD_NAMES = {'HeadYaw': "he1",
-                   'HeadPitch': "he2",
-                   'LShoulderPitch': "lae1",
-                   'LShoulderRoll': "lae2",
-                   'LElbowYaw': "lae3",
-                   'LElbowRoll': "lae4",
-                   'LHipYawPitch': "lle1",
-                   'LHipRoll': "lle2",
-                   'LHipPitch': "lle3",
-                   'LKneePitch': "lle4",
-                   'LAnklePitch': "lle5",
-                   'LAnkleRoll': "lle6",
-                   'RShoulderPitch': "rae1",
-                   'RShoulderRoll': "rae2",
-                   'RElbowYaw': "rae3",
-                   'RElbowRoll': "rae4",
-                   'RHipYawPitch': "rle1",
-                   'RHipRoll': "rle2",
-                   'RHipPitch': "rle3",
-                   'RKneePitch': "rle4",
-                   'RAnklePitch': "rle5",
-                   'RAnkleRoll': "rle6"}
-
 class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
